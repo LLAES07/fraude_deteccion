aaee906a289055e073be05a8c9354b4bb92fe394
LLAES07 <llanos.k.geology@gmail.com>
2026-04-07 14:38:34 -0400

    agregado @dataclass en data ingestion

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -1,7 +1,22 @@
-# Importe de librerias
+import os
+import sys
 from src.logger import logging
 from src.exception import CustomException
 import pandas as pd
 from dataclasses import dataclass
 
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    valid_data_path: str = os.path.join('artifacts', 'valid.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
 
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+if __name__=='__main__':
+    obj = DataIngestion()
+
+    
