83eb9e04df4d6c2ba75f4b6d3f568c2618cdd031
LLAES07 <llanos.k.geology@gmail.com>
2026-04-07 15:08:42 -0400

    Importada librerias en data_transformation.py

---
+++ src/Components/data_transformation.py
@@ -0,0 +1,12 @@
+# Carga de librerias
+
+import os
+import re
+import numpy as np
+from dataclasses import dataclass
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
+from src.exception import CustomExecption
+from src.logger import logging
