5306d75451ed487f62df769a585f4ba050a56671
LLAES07 <llanos.k.geology@gmail.com>
2026-04-13 11:54:39 -0400

    Agregado path del motel.pkl

--- src/Components/model_trainer.py
+++ src/Components/model_trainer.py
@@ -11,3 +11,20 @@ from src.utils import save_object
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def iniciador_modelo_entrenamiento(self, train_array, test_array):
+        try:
+            logging.info("Dividiendo los datos de entrenamiento y prueba (features y target)")
+            
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+        finally:
+            pass
