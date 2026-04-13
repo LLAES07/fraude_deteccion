c6028bfa2a450afcabb7782d4a42931fb1910a37
LLAES07 <llanos.k.geology@gmail.com>
2026-04-13 12:17:48 -0400

    Agregado todo lo demas

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -17,5 +17,30 @@ class TrainPipeline:
             # Ingestion
             data_ingestion = DataIngestion()
             train_data_path, test_bots_path, test_norm_path = data_ingestion.iniciador_ingesta()
-        finally:
-            pass
+            
+            # Transformacion
+            data_transformation = DataTransformation()
+            train_arr, test_bots_arr, test_norm_arr, preprocessor_path = data_transformation.initiate_data_transformation(
+                train_path=train_data_path,
+                test_bots_path=test_bots_path,
+                test_norm_path=test_norm_path
+            )
+            
+            # Entrenamiento
+            model_trainer = ModelTrainer()
+            metrics = model_trainer.iniciador_modelo_entrenamiento(
+                train_array=train_arr,
+                test_bots_array=test_bots_arr,
+                test_norm_array=test_norm_arr
+            )
+            
+            logging.info(f"Pipeline completado. Métricas: {metrics}")
+            print(f"Métricas Finales: {metrics}")
+            return metrics
+            
+        except Exception as e:
+            raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    pipeline = TrainPipeline()
+    pipeline.run_pipeline()
